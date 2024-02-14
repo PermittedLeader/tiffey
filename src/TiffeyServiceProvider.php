76dacefb06b3bfaf07dcdabb116b22078ede7e6b
Myles Smith <hello@itsmyl.es>
2024-02-14 17:06:23 +0000

    Update TiffeyServiceProvider.php

--- src/TiffeyServiceProvider.php
+++ src/TiffeyServiceProvider.php
@@ -9,7 +9,8 @@ class TiffeyServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__."/../config/tiffey.php" => config_path('tiffey.php')
+            __DIR__."/../config/tiffey.php" => config_path('tiffey.php'),
+            __DIR__."/../resources/views" => resource_path('views/vendor/tiffey')
         ]);
         
         $this->loadViewsFrom(__DIR__.'/../resources/views/','tiffey');
