1cc4d29b0cca0ed0c68e4ae42b3d7f3bb0ab6da1
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-03-06 16:00:23 +0000

    make labels hideable on checkboxes

--- resources/views/components/input/checkbox.blade.php
+++ resources/views/components/input/checkbox.blade.php
@@ -1,14 +1,17 @@
-@props(['label','hint'=>false,'name'])
+@props(['label','hint'=>false,'name','inBlock'=>false])
 @php
 $randId = $name.bin2hex(random_bytes(2)); 
 @endphp
 <div class="flex flex-row items-center ml-1">
     <input type="checkbox" id="{{ $randId }}" name="{{ $name }}" {{ $attributes->merge(['class'=>'my-2 mr-3 p-2 '.config('tiffey.border-color')]) }}>
+    @if($inBlock)
     <label class="font-bold" for="{{ $randId }}">
         {{ $label }}
         @if($hint)
             <x-tiffey::input.hint class="ml-2">{{ $hint }}</x-tiffey::hint>
         @endif
     </label>
-
+    @else
+    <span class="sr-only">{{ $label }}</span>
+    @endif
 </div>
