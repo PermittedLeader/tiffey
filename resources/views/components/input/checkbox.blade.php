9e90b7abd9da28f7ad74d71757d740b10ff4a5f7
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-03-12 12:36:26 +0000

    adding ID to checkboxes

--- resources/views/components/input/checkbox.blade.php
+++ resources/views/components/input/checkbox.blade.php
@@ -1,11 +1,16 @@
-@props(['label','hint'=>false,'name','inBlock'=>false])
+@props(['label','hint'=>false,'name'=>false,'inBlock'=>false, 'id'=>false])
 @php
-$randId = $name.bin2hex(random_bytes(2)); 
+    if(!$name){
+        $name = Str::camel($label);
+    }
+    if(!$id){
+        $id= Str::snake(preg_replace('/[^A-Za-z0-9_]/',' ',$name));
+    }
 @endphp
 <div class="flex flex-row items-center ml-1">
-    <input type="checkbox" id="{{ $randId }}" name="{{ $name }}" {{ $attributes->merge(['class'=>'my-2 mr-3 p-2 '.config('tiffey.border-color')]) }}>
+    <input type="checkbox" id="{{ $id }}" name="{{ $name }}" {{ $attributes->merge(['class'=>'my-2 mr-3 p-2 '.config('tiffey.border-color')]) }}>
     @if(!$inBlock)
-    <label class="font-bold" for="{{ $randId }}">
+    <label class="font-bold" for="{{ $id }}">
         {{ $label }}
         @if($hint)
             <x-tiffey::input.hint class="ml-2">{{ $hint }}</x-tiffey::hint>
