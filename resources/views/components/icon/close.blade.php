3857a6d111a4ed6ab399ab917d51a0f57da783a7
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-03-19 07:45:41 +0000

    adding close button and inline menu

---
+++ resources/views/components/icon/close.blade.php
@@ -0,0 +1,2 @@
+@props(['icon'=>'fa-solid fa-xmark','label'=>'Close'])
+<x-tiffey::icon icon="{{ $icon }}" label="{{ $label }}" />
---
+++ resources/views/components/menu/index.blade.php
@@ -0,0 +1,28 @@
+<div x-data="{menuOpen: false}" @click.outside="menuOpen = false" class="relative">
+    @isset($button)
+        <div @click="menuOpen = !menuOpen">
+            {{ $button }}
+        </div>
+        
+    @endisset
+
+    <div x-show="menuOpen"
+            x-transition:enter="transition ease-out duration-200"
+            x-transition:enter-start="transform opacity-0 scale-95"
+            x-transition:enter-end="transform opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-75"
+            x-transition:leave-start="transform opacity-100 scale-100"
+            x-transition:leave-end="transform opacity-0 scale-95"
+            style="display: none;"
+            class="absolute z-0 -right-0 -top-2 md:-top-3 md:z-50 mt-0 md:mt-1 shadow-lg bg-white text-black dark:text-white dark:bg-black text-sm {{ config('tiffey.rounded') }} ">
+        <div class="{{ config('tiffey.rounded') }} flex flex-row gap-2 p-2">
+            {{ $slot }}
+            <span @click="menuOpen = false">
+                <x-tiffey::button>
+                    <x-tiffey::icon.close />
+                </x-tiffey::button>
+            </span>
+            
+        </div>
+    </div>
+</div>
