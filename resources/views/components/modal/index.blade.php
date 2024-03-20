2ccfcf67a7077ea14ea3657c04c2fc6761ec8589
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-03-20 19:35:23 +0000

    renaming footer

--- resources/views/components/modal/index.blade.php
+++ resources/views/components/modal/index.blade.php
@@ -61,10 +61,10 @@
                     <div class="p-8">
                         {{ $slot }}
 
-                        @isset($footer)
+                        @isset($footerActions)
                         <div class="p-4 py-4 px-4 -mx-8 -mb-8 mt-8 bg-slate-100">
                             <div class="flex justify-end gap-4">
-                                {{ $footer }}
+                                {{ $footerActions }}
                             </div>
                         </div>
                         @endisset
