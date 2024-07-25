3e73acbe82e3c6678cd4852f3e0d0194b218dfdf
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-07-25 16:02:10 +0100

    Update footer.blade.php

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="flex flex-row justify-between">
     <div>{{ config('app.name') }}</div> 
-    <div>Using Tiffey layout theme</div>
+    <div class="text-right">Using Tiffey layout theme</div>
 </div>
