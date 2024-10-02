6691aab4bd52bbf76e44423503acc22dac8c7f62
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-10-02 21:01:40 +0100

    adding new icons for true and false

---
+++ resources/views/components/icon/false.blade.php
@@ -0,0 +1,2 @@
+@props(['icon'=>'fa-solid fa-xmark','label'=>'False'])
+<x-tiffey::icon icon="{{ $icon }}" label="{{ $label }}" />
---
+++ resources/views/components/icon/true.blade.php
@@ -0,0 +1,2 @@
+@props(['icon'=>'fa-solid fa-check','label'=>'True'])
+<x-tiffey::icon icon="{{ $icon }}" label="{{ $label }}" />
