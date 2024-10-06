66d95d874c5f3b37dd3d27270e205fe0b7741edb
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-10-06 11:56:46 +0100

    extending delay on button animation to 200ms

--- resources/views/components/form-button.blade.php
+++ resources/views/components/form-button.blade.php
@@ -21,11 +21,11 @@
     }
 @endphp
 <button {{ $attributes->merge(['class'=>'relative data-[loading]:pointer-events-none inline-block my-auto text-center '.config('tiffey.rounded').' hover:bg-opacity-75 hover:shadow-inner font-bold dark:border dark:border-'.$text.' text-'.$text.' '.$color.' '.$sizeClasses]) }} role="button" wire:loading.attr="data-loading">
-    <div class="[[data-loading]>&]:opacity-0 transition-opacity delay-100">
+    <div class="[[data-loading]>&]:opacity-0 transition-opacity delay-200">
         {{ $slot }}
     </div>
     
-    <div class="[[data-loading]>&]:opacity-100 transition-opacity delay-100 opacity-0 absolute inset-0 flex items-center justify-center">
+    <div class="[[data-loading]>&]:opacity-100 transition-opacity delay-200 opacity-0 absolute inset-0 flex items-center justify-center">
         <x-tiffey::icon.loading />
     </div>
 </button>
