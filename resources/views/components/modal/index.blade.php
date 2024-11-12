91d6970bab41e37113fe1ad582a982f27376414c
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-11-12 21:11:22 +0000

    changing modal footer colour

--- resources/views/components/modal/index.blade.php
+++ resources/views/components/modal/index.blade.php
@@ -62,7 +62,7 @@
                         {{ $slot }}
 
                         @isset($footerActions)
-                        <div class="p-4 py-4 px-4 -mx-8 -mb-8 mt-8 bg-slate-100">
+                        <div class="p-4 py-4 px-4 -mx-8 -mb-8 mt-8 border-t border-grey-500/50">
                             <div class="flex justify-end gap-4">
                                 {{ $footerActions }}
                             </div>
